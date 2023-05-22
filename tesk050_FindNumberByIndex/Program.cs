960cae8698e37996cf45e874b0f313696537bcba
Roman <roma.klimov10.94@gmail.com>
2023-05-23 08:37:24 +1100

    tesk052

---
+++ tesk050_FindNumberByIndex/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+int FindNumbers(int[,] array, int index1, int index2)
+{
+    int yourNumber = -1;
+    if(index1 < 0 || index2 < 0)
+    {
+        Console.WriteLine("один из индексов меньше \"0\" ");
+        return yourNumber;
+    }
+    if(index1 > array.GetLength(0) - 1 || index2 > array.GetLength(1) - 1)
+    {
+        Console.WriteLine($"число с индексами {index1}, {index2} нет в массиве");
+        return yourNumber;
+    }
+    yourNumber = array[index1, index2];
+    return yourNumber;
+}
+
+
+
+void PrintArray2D(int[,] array)    // метод печати двумергного  массива 
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] array = new int[2,2]{{1,2},{3,4}};
+int result = FindNumbers(array, 1, 10);
+PrintArray2D(array);
+Console.WriteLine(result);
---
+++ tesk052AverageSr/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+double[] FindAvarage(int[,] array)
+{
+    double[] mass = new double[array.GetLength(1)];
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        int sum = 0;
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i,j];             
+        }
+        double result = (double) sum / array.GetLength(1); // (double - строгая конвертация , все что после скобок приводится к типу данных указанных в них)
+        mass[j] = result;
+    }
+    return mass;
+}
+
+int[,] FillArray2D(int[,] array, int minValue, int maxValue)
+{
+    Random randomDigit = new Random();
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = randomDigit.Next(minValue, maxValue +1);
+        }
+    }
+    return array;
+}
+
+void PrintArray2D(int[,] array)    // метод печати двумергного  массива 
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+
+int[,] mass = new int[3,4];
+FillArray2D(mass, 1, 9);
+Console.WriteLine("");
+PrintArray2D(mass);
+ double[] newMass = FindAvarage(mass);
+ for(int i = 0; i < newMass.GetLength(0); i++)
+ {
+    Console.Write($"{newMass[i]}, ");
+ }
+Console.WriteLine("");
+ 
