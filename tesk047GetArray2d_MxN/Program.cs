4c689d3b5255bb0fa1230daa627cc39adfd7ae97
Roman <roma.klimov10.94@gmail.com>
2023-05-23 23:04:51 +1100

    tesk 047

--- tesk047GetArray2d_MxN/Program.cs
+++ tesk047GetArray2d_MxN/Program.cs
@@ -5,10 +5,41 @@
 // 8 7,8 -7,1 9
 
 
-//Random rnd = new Random()
+//Random rnd = new Random()    rnd берем больше  в 10 раз , бля заполнения массива числами  в промежутке от 0 до 100 - пишем 1001 от 0 до 1000 - пишем 10 000 и тд..(?)
 
-array[i,j] = new Random().Next(1001) / (double)10;
+double[,] GetArray2D(int rows, int columns)
+{
+    double[,] array = new double[rows, columns];
+    Random rnd = new Random();
+    {
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+            for(int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i,j] = rnd.Next(10000) / (double)10; // строгая типизация привести 10 к вещественному чилсу (?)
+            }
+        }
+        return array;
+    }
+}
+
+void PrintArray2D(double[,] array)    // метод печати двумергного  массива 
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
 
 
+//array[i,j] = new Random().Next(1001) / (double)10;
 
+int rows = 5; int columns = 3;
+double[,] array = GetArray2D(rows, columns);
 
+PrintArray2D(array);
